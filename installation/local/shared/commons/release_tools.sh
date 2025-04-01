2fe59b042ad72592b86afb01da715c3928a2874e
adrianciu <adrian.ciu@codemart.ro>
2025-04-01 15:46:13 +0300

    VT-99: script for installing virtual box on ubuntu 24.04 runner

--- installation/local/shared/commons/release_tools.sh
+++ installation/local/shared/commons/release_tools.sh
@@ -1,11 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
+# Update and reinstall essential packages
 sudo apt update
-curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor > oracle_vbox_2016.gpg$ curl https://www.virtualbox.org/download/oracle_vbox.asc | gpg --dearmor > oracle_vbox.gpg$ sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/$ sudo install -o root -g root -m 644 oracle_vbox.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-sudo apt updatesudo apt install -y linux-headers-$(uname -r) dkmssudo apt install virtualbox-7.0 -y
+sudo apt install -o DPkg::Options::=--force-confold -y -q --reinstall \
+    bzip2 ca-certificates g++ gcc make gfortran git gzip lsb-release \
+    patch python3 python3-pip tar unzip xz-utils zstd gnupg2 vim curl rsync
 
+# Install VirtualBox 7.1.6
+echo "Installing VirtualBox..."
+# Download VirtualBox .deb
+curl -O https://download.virtualbox.org/virtualbox/7.1.6/virtualbox-7.1_7.1.6-167084~Ubuntu~noble_amd64.deb
 
+# Install the .deb package
+sudo dpkg -i virtualbox-7.1_7.1.6-167084~Ubuntu~noble_amd64.deb || true
+# Fix broken dependencies if any
+sudo apt-get install -f -y
+# Install build tools and kernel headers
+sudo apt install -y build-essential dkms linux-headers-$(uname -r)
+# Configure VirtualBox kernel modules
+sudo /sbin/vboxconfig
+# Output version to verify install
 echo "VirtualBox version:"
 vboxmanage --version
+
+# Install Vagrant
+echo "Installing Vagrant..."
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
+
+echo "Vagrant version:"
+vagrant --version
+vboxmanage --version
+
+
