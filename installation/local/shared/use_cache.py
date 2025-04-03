b73788543bc8d48061c27e0aa022b0850d6a75c5
adrianciu <adrian.ciu@codemart.ro>
2025-04-03 16:23:47 +0300

    VT-99: improved scripts to be compatible with VM and ci pipeline

--- installation/local/shared/use_cache.py
+++ installation/local/shared/use_cache.py
@@ -10,17 +10,6 @@ from vbt_config import user, home_path, set_env_vars, install_dir, data_dir, bas
     concretization_dir, vbt_env, ebrains_repo, vbt_spack_env_name
 
 if __name__ == "__main__":
-    if user:
-        set_env_vars()
-        spack_path = str(data_dir / 'spack')
-        append_command_to_file(command=f'sudo chown -R {user}:{user} {spack_path}',
-                               file_path=f'{home_path}/.bashrc')
-        ebrains_path = str(data_dir / 'ebrains-spack-builds')
-        append_command_to_file(command=f'sudo chown -R {user}:{user} {ebrains_path}',
-                               file_path=f'{home_path}/.bashrc')
-        append_command_to_file(command=f'sudo chown -R {user}:{user} {data_dir / {vbt_spack_env_name} }',
-                               file_path=f'{home_path}/.bashrc')
-
     spack_config = SpackConfig(env=vbt_env,
                                repos=[ebrains_repo],
                                install_dir=install_dir,
@@ -31,11 +20,25 @@ if __name__ == "__main__":
                                gpg=None,
                                use_spack_global=False,
                                cache_version_build=os.getenv('BUILDCACHE_OCI_VERSION'),
-                               cache_version_concretize=os.getenv('CONCRETIZE_OCI_VERSION'))
+                               cache_version_concretize=os.getenv('CONCRETIZE_OCI_VERSION'),
+                               bashrc_path=bashrc_path)
 
     spack_operation = SpackOperationCreator.get_spack_operator(spack_config, use_cache=True)
     spack_operation.install_spack('0.23.1', bashrc_path=bashrc_path)
+    if user:
+        set_env_vars()
+        spack_path = str(install_dir / 'spack')
+        append_command_to_file(command=f'sudo chown -R {user}:{user} {spack_path}',
+                               file_path=f'{home_path}/.bashrc')
+        ebrains_env_path = str(data_dir / 'ebrains-spack-builds')
+        append_command_to_file(command=f'sudo chown -R {user}:{user} {ebrains_env_path}',
+                               file_path=f'{home_path}/.bashrc')
+        vbt_env_path = str(data_dir / vbt_spack_env_name)
+        append_command_to_file(command=f'sudo chown -R {user}:{user} {vbt_env_path}',
+                               file_path=f'{home_path}/.bashrc')
     spack_operation.setup_spack_env()
+    spack_operation.reindex()
+    spack_operation.update_buildcache_index(spack_operation.spack_config.buildcache_dir)
     spack_operation.concretize_spack_env()
     spack_operation.install_packages(os.cpu_count())
     if not check_installed_all_spack_packages(data_dir / vbt_env.name, spack_operation):
