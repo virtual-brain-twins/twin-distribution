33d43ec6b089fcf8adfb82b08f439c0bcc39dd0c
adrianciu <adrian.ciu@codemart.ro>
2025-04-07 17:14:33 +0300

    VT-99: testing running vagrant vm from another vagrant vm

--- installation/local/shared/commons/release_tools.sh
+++ installation/local/shared/commons/release_tools.sh
@@ -30,8 +30,7 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vagrant
 
-echo "Vagrant version:"
-vagrant --version
-vboxmanage --version
+cd ../VM_release
+vagrant up
 
 
