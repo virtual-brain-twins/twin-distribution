ca82946edf6e498ee85a07cb595ec3d61127b192
adrianciu <adrian.ciu@codemart.ro>
2025-04-10 09:58:47 +0300

    VT-99: ci for release

--- installation/local/release/release_vagrant_box_tools.sh
+++ installation/local/release/release_vagrant_box_tools.sh
@@ -26,14 +26,3 @@ echo "Installing Vagrant..."
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vagrant
-
-# Output version to verify install
-echo "VirtualBox version:"
-vboxmanage --version
-echo "Vagrant version:"
-vagrant --version
-
-cd ./shared/VM_release
-vagrant up
-
-
