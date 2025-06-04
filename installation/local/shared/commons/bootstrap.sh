17a127ebb84592ffa529ab532eb75aa7e67c9c0e
adrianciu <adrian.ciu@codemart.ro>
2025-06-04 09:27:03 +0300

    JSC_HPC_BUILD jobs: cache in docker: python:3.12

--- installation/local/shared/commons/bootstrap.sh
+++ installation/local/shared/commons/bootstrap.sh
@@ -2,7 +2,7 @@
 set -e
 
 apt update && apt install -y --no-install-recommends \
-    bzip2 ca-certificates g++ gcc make gfortran git gzip patch \
+    bzip2 ca-certificates g++ gcc make gfortran git gzip patch lsb-release\
     python3 python3-pip tar unzip xz-utils zstd gnupg2 curl rsync jq \
     flex bison libgmp-dev libmpfr-dev libmpc-dev texinfo libisl-dev libzstd-dev  \
     gnupg gpg gpg-agent
