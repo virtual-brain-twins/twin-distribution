1f892e52fe6fec46401a292db5af8b0165306c0c
adrianciu <adrian.ciu@codemart.ro>
2025-06-04 09:41:13 +0300

    JSC_HPC_BUILD jobs: cache in docker: python:3.12

--- installation/local/shared/create_cache.py
+++ installation/local/shared/create_cache.py
@@ -29,4 +29,6 @@ if __name__ == "__main__":
     spack_operation.concretize_spack_env()
     spack_operation.install_packages(os.cpu_count())
     if not check_installed_all_spack_packages(Path('../').resolve() / data_dir / vbt_env.name, spack_operation):
-        sys.exit(-1)
+        print('false')
+    else:
+        print('true')
